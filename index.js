bec952a9e3a050bcef8133b294a3e311e45f2180
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2024-01-09 17:17:24 -0300

    Correção de bug; alteração na hierarquia das rotas no arquivo pois a requisição não estava sendo direcionado para a rota correta.

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ import { retornaColecaoCompleta, verificaParametroVazio,
 const app = express();
 
 app.get('/historicoIPCA', (req, res) => {
-    let anoRequisitado = (req.query.ano);
-    
+    let anoRequisitado = (req.query.ano);   
     if (verificaParametroVazio(anoRequisitado)){
         const resultado = retornaColecaoCompleta();
         res.json(resultado);
@@ -21,35 +20,37 @@ app.get('/historicoIPCA', (req, res) => {
     }    
 });
 
-app.get('/historicoIPCA/:id', (req, res) => {
-    let idRequisitado = retornaRegistroID(req.params.id);
-
-    if (idRequisitado) {
-        res.json(idRequisitado);
-    } else {
-        res.status(404).send({"erro": "Elemento não encontrado"});
-    }
-});
-
 app.get('/historicoIPCA/calculo', (req, res) => {
     const valorCalcular = parseFloat(req.query.valor);
     const dataInicialMes = parseInt(req.query.mesInicial);
     const dataInicialAno = parseInt(req.query.anoInicial);
     const dataFinalMes = parseInt(req.query.mesFinal);
     const dataFinalAno = parseInt(req.query.anoFinal);
-
     if (validaParametrosCalculo(valorCalcular, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno)) {
             if (!(dataInicialAno > dataFinalAno)) {
                 const resultado = retornaCalculoReajuste(valorCalcular, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno);
                 res.json({"valor reajustado" : resultado});
             } else {
-                res.json({"parametros": "invalidos"});
-                
+                res.json({"parametros": "invalidos"});                
             }         
     } else {
         res.json({"parametros": "invalidos"});
+    }    
+});
+
+app.get('/historicoIPCA/:id', (req, res) => {
+    let idRequisitado = parseInt(req.params.id);
+    if (isNaN(idRequisitado)) {
+        res.status(404).send({erro: "ID inválido"});
+        return;
     }
-    
+
+    const elemento = retornaRegistroID(idRequisitado);
+    if (elemento) {
+        res.json(elemento);
+    } else {
+        res.status(404).json({erro: "Elemento não encontrado"})
+    }
 });
 
 app.listen(8080, () => {
